d51fc17e631f0c9d27b4f27109eb3d50ae506487
MTLY-1610 <larrouy.m@me.com>
2020-04-22 13:27:56 +0200

    prompt if msg form exist

--- public/script.js
+++ public/script.js
@@ -3,10 +3,12 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('what is your little name pumpkin?')
-appendMessage('you joined')
-socket.emit('new-user', name)
 
+if (messageForm !=null){
+    const name = prompt('what is your little name pumpkin?')
+    appendMessage('you joined')
+    socket.emit('new-user', name)
+}
 
 
 socket.on('chat-message', data =>{
